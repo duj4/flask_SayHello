39db31d6892f1fd611ab0aa76d7520ceeba36810
duj4 <boyu.du@yahoo.com>
2019-12-30 15:36:20 +0800

    7.1

---
+++ sayhello/__init__.py
@@ -0,0 +1,14 @@
+# 在Python中，每一个有效的Python文件都是模块，每一个包含__init__.py文件的文件夹都被视作包
+# __init__.py文件通常被称作构造文件，文件可以为空，也可以用来放置包的初始化代码，当包或包内的模块被导入时，构造文件将被自动执行
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# 在单脚本中创建程序实例时，我们传入__name__变量作为Flask类构造方法的import_name参数值，因为Flask通过这个值来确认程序路径
+# 当用包组织代码时，为了确保其他扩展或测试框架获得正确的路径值，最好以硬编码的形式写出包名称作为程序名称
+app = Flask('flask_SayHello')
+app.config.from_pyfile('settings.py')
+app.jinja_env.trim_blocks = True # 删除jinja2语句后的第一个空行
+app.jinja_env.lstrip_blocks = True # 删除jinja2语句所在行之前的空格和制表符（tab）
+
+db = SQLAlchemy(app)
---
+++ sayhello/settings.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+from sayhello import app
+
+DEBUG = True
+HOST = '0.0.0.0'
+PORT = '5000'
+
+# SQLite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+dev_db = prefix + os.path.join(os.path.dirname(app.root_path), 'data.db')
+
+SECRET_KEY = 'secret string'
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', dev_db)
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
