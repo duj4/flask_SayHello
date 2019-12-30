696633cab0fdfe7185fc4fb4f25cf9ce1d4b9422
duj4 <boyu.du@yahoo.com>
2019-12-30 22:28:14 +0800

    index.html

--- sayhello/models.py
+++ sayhello/models.py
@@ -7,4 +7,5 @@ class Message(db.Model):
     body = db.Column(db.String(200))
     name = db.Column(db.String(20))
     # 时间戳
+    # 如果传入的时间戳不是方法对象而是方法本身，那么在加载模块时这个函数就会被执行，时间戳的值就不正确
     timestamp = db.Column(db.DateTime, default=datetime.now, index=True)
