dbed148b4af5a392cb30afadc79c4c95d44a03e0
duj4 <boyu.du@yahoo.com>
2020-01-01 15:20:10 +0800

    7.4

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -2,8 +2,12 @@
 # __init__.py文件通常被称作构造文件，文件可以为空，也可以用来放置包的初始化代码，当包或包内的模块被导入时，构造文件将被自动执行
 
 from flask import Flask
+# 扩展Bootstrap-Flask内置了可以快速渲染Bootstrap样式HTML组件的宏，并提供了内置的Bootstrap资源
+from flask_bootstrap import Bootstrap
+# Moment.js是一个用于处理时间和日期的开源JS库，它可以对时间和日期进行各种方式的处理
+# 它会根据用户电脑中的时区设置在客户端使用JS来渲染时间和日期，而且还提供了丰富的时间渲染格式支持
+from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-
 
 # 在单脚本中创建程序实例时，我们传入__name__变量作为Flask类构造方法的import_name参数值，因为Flask通过这个值来确认程序路径
 # 当用包组织代码时，为了确保其他扩展或测试框架获得正确的路径值，最好以硬编码的形式写出包名称作为程序名称
@@ -13,3 +17,5 @@ app.jinja_env.trim_blocks = True # 删除jinja2语句后的第一个空行
 app.jinja_env.lstrip_blocks = True # 删除jinja2语句所在行之前的空格和制表符（tab）
 
 db = SQLAlchemy(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
--- sayhello/static/js/script.js
+++ sayhello/static/js/script.js
@@ -1,5 +1,7 @@
 $(function () {
     function render_time() {
+        // 这个moment时Moment.js提供的
+        // $(this).data('timestamp')获取了对应元素的data-timestamp属性值
         return moment($(this).data('timestamp')).format('lll')
     }
     $('[data-toggle="tooltip"]').tooltip(
