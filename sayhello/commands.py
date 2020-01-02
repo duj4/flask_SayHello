971a35f478ef93fdb1e1fa086fa17b500acc1219
duj4 <boyu.du@yahoo.com>
2020-01-02 11:17:36 +0800

    mark templates as template folder; 7.5

--- sayhello/commands.py
+++ sayhello/commands.py
@@ -1,6 +1,8 @@
 import click
 from sayhello import app, db
 from sayhello.models import Message
+# 用于生成虚拟数据
+from faker import Faker
 
 @app.cli.command()
 @click.option('--drop', is_flag=True, help='Create after drop.')
@@ -12,3 +14,22 @@ def initdb(drop):
         click.echo("Drop tables.")
     db.create_all()
     click.echo("Initialized database.")
+
+@app.cli.command()
+@click.option('--count', default=20, help='Quantity of messages, default is 20.')
+def forge(count):
+    """Generate fake messages."""
+    db.drop_all()
+    db.create_all()
+
+    fake = Faker()
+    click.echo("Working...")
+
+    for i in range(count):
+        message = Message(name=fake.name(),
+                          body=fake.sentence(),
+                          timestamp=fake.date_time_this_year())
+        db.session.add(message)
+
+    db.session.commit()
+    click.echo("Created %d fake messages." % count)
